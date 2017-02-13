9bdf28e6ce0ea1a191c8a1afc30f281841b50be1
sunjianp <2388184579@qq.com>
2017-02-13 15:31:59 +0800

    个人简历

---
+++ data/timeAxis.js
@@ -0,0 +1,36 @@
+
+/*		工作时光轴
+	*   {
+	*       startTime: @value String            *         工作开始时间  格式为：2016.10.09
+	*       endTime: @value String 			    *         工作结束时间  格式为：2016.10.09
+	*       companyLogo: @value String 	        *         公司log
+			companyName: @value String          *         公司名称
+	*       companyWebsite: @value String       []        公司网址       格式为：http://www.miaov.com
+	*       jobPost: @value String              *         工作职位
+	*       jonTask: @value String       		*         负责任务
+	*       jobContent: @value String         	*         工作具体内容
+	*
+	*
+	*   }
+*/
+
+var timeAxisArr = [
+		{
+		    startTime:"2015.03.09",
+			endTime:"2015.12.01",
+			companyLogo:'images/icon/com_logo1.gif',
+			companyNam:"黄山视听在线网络有限公司",
+			companyWebsite:"http://www.zxxk.com",
+			jobPost:"前端开发工程师",
+			jonTask:"编写了各种专题页的静态页面",
+			jobContent:"包括：根据UI设计图，专题页静态页面的编写，以及利用jQuery和js完成了一些动态效果，其中包括导航的跳转，侧边框的显示隐藏，侧边框回到顶部功能，以及滑过变色等效果。",
+		},
+		{
+		    startTime:"2015.12.15",
+			endTime:"2017.2",
+			companyNam:"杭州昆汀科技股份有限公司",
+			companyLogo:'images/icon/com_logo2.png',
+			companyWebsite:"http://www.miaov.com",
+			jobContent:"内容包括：从事WEB前端开发、移动端页面的开发、再到原生JAVASCRIPT、Ajax等内容，以上技术比较熟练，还学习了一些类库，如JQuery，还学习到了版本控制工具git和svn、了解到了一些类库和框架如Vue.js、react.js、Node.js等",
+		}
+]
